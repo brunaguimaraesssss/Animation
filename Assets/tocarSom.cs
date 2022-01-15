2bdac90f2c6621808615fc176deec1cf152dd092
Bruna Guimarães <62905224+brunaguimaraesssss@users.noreply.github.com>
2022-01-15 16:46:25 -0300

    Initial commit

---
+++ Assets/tocarSom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tocarSom : MonoBehaviour
+{
+    private AudioSource MeuAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeuAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FalaAi()
+    {
+        MeuAudio.Play();
+    }
+}
